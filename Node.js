74c91d26827b64838fca0f95662afbf1a6967003
F3rNaNDEZ57 <98757988+F3rNaNDEZ57@users.noreply.github.com>
2023-10-21 09:55:02 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.post('/receiveSMS', (req, res) => {
-    const incomingMessage = req.body;
-    console.log(incomingMessage); 
-    res.sendStatus(200);
+app.post('/sendSMS', async (req, res) => {
+  const { message, numbers } = req.body;
+  try {
+    const response = await axios.post('https://api.dialog.lk/sms/send', {
+      message: message,
+      destinationAddresses: numbers,
+      password: "ade49a19b550a168415764f273f49fc5",
+      applicationId: "APP_064990"
+    });
+    res.json({ success: true, response: response.data });
+  } catch (error) {
+    console.error("Error sending SMS:", error);
+    res.status(500).json({ success: false, error: "Unable to send SMS" });
+  }
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
